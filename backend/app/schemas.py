d54ce6409033f030cf631cd9420a9de5cde5d778
Rancy21 <rancyontsandaga@gmail.com>
2026-07-07 13:05:06 +0200

    update: Product and Order schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,13 +1,27 @@
+import uuid
+from datetime import datetime
+from decimal import Decimal
 from typing import Optional
 
 from pydantic import BaseModel
+from pydantic.fields import Field
+
+
+class UserRole(BaseModel):
+    name: str
+
+    class Config:
+        from_attributes = True
 
 
 class User(BaseModel):
+    id: int
     username: str
     full_name: Optional[str] = None
     email: Optional[str] = None
     disabled: Optional[bool] = None
+
+    roles: list[UserRole]
 
     class Config:
         from_attributes = True
@@ -39,6 +53,7 @@ class Product(BaseModel):
 
 
 class ProductCreate(BaseModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
     name: str
     category: str
     description: str
@@ -50,3 +65,25 @@ class ProductCreate(BaseModel):
     protein: Optional[float] = None
     fat: Optional[float] = None
     image_url: Optional[str] = None
+
+
+class OrderItemCreate(BaseModel):
+    order_id: int
+    product_id: str
+    price_at_purchase: Decimal
+    quantity: Decimal
+
+
+class OrderResponse(BaseModel):
+    user_name: str
+    total_price: Decimal
+    status: str
+    created_at: datetime
+
+
+class OrderItem(BaseModel):
+    id: int
+    order_id: int
+    product_name: str
+    quantity: Decimal
+    price_at_purchase: Decimal
