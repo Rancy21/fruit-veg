52cb3cfddb5932466fd7c85571fa1c53a7a01567
Rancy21 <rancyontsandaga@gmail.com>
2026-07-07 13:03:10 +0200

    Added UserRole and UUIDConvertor

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,28 @@
-class OrderStatus:
-    PENDING: str = "PENDING"
-    COMPLETED: str = "COMPLETED"
-    CANCELLED: str = "CANCELLED"
+from enum import Enum
+from uuid import UUID
+
+from starlette.convertors import Convertor
+
+
+class OrderStatus(str, Enum):
+    PENDING = "PENDING"
+    COMPLETED = "COMPLETED"
+    CANCELLED = "CANCELLED"
+
+
+class UserRole(str, Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+
+class UUIDConvertor(Convertor):
+    """UUID Convertor with a regex"""
+
+    # Standard UUID regex pattern
+    regex = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+
+    def convert(self, value: str) -> UUID:
+        return UUID(value)
+
+    def to_string(self, value: UUID) -> str:
+        return str(value)
